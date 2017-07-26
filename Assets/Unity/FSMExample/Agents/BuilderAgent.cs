fffb162ed561d9b2d3c194330c84bbe39023d6ee
Shiv Kumar <shivpkumar@gmail.com>
2017-07-26 10:00:16 -0700

    Add more goals for builder and randomize selection

--- Assets/Unity/FSMExample/Agents/BuilderAgent.cs
+++ Assets/Unity/FSMExample/Agents/BuilderAgent.cs
@@ -1,6 +1,19 @@
-﻿namespace ReGoap.Unity.FSMExample.Agents
-{
-    public class BuilderAgent : ReGoapAgentAdvanced<string, object>
-    {
+﻿using System.Collections.Generic;
+
+namespace ReGoap.Unity.FSMExample.Agents {
+    public class BuilderAgent : ReGoapAgentAdvanced<string, object> {
+        protected override bool CalculateNewGoal(bool forceStart = false) {
+            ReprioritizeGoals();
+            base.CalculateNewGoal(forceStart);
+            return true;
+        }
+
+        private void ReprioritizeGoals() {
+            List<ReGoapGoal<string, object>> goals = new List<ReGoapGoal<string, object>>(GetComponents<ReGoapGoal<string, object>>());
+
+            foreach (var goal in goals) {
+                goal.Priority = UnityEngine.Random.Range(1.0f, 10.0f);
+            }
+        }
     }
-}
+}  
