57d1a4caf4388e6f041505e5eb31a886a720f5ed
Shiv Kumar <shivpkumar@gmail.com>
2017-07-26 17:07:26 -0700

    Sensor test: run away from ScaryAgent

--- Assets/Unity/FSMExample/Agents/BuilderAgent.cs
+++ Assets/Unity/FSMExample/Agents/BuilderAgent.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ReGoap.Unity.FSMExample.Agents {
     public class BuilderAgent : ReGoapAgentAdvanced<string, object> {
+        public float fleeDistance = 10.0f;
+        bool forceWander = false;
+
+        protected override void Update() {
+            base.Update();
+
+            float? sqrDistanceToScaryAgent = memory.GetWorldState().Get("sqrDistanceToScaryAgent") as float?;
+
+            if (sqrDistanceToScaryAgent.HasValue && sqrDistanceToScaryAgent < fleeDistance * fleeDistance && currentGoal.GetPriority() < 100) {
+                forceWander = true;
+                CalculateNewGoal(true);
+            }
+        }
+
         protected override bool CalculateNewGoal(bool forceStart = false) {
             ReprioritizeGoals();
-            base.CalculateNewGoal(forceStart);
-            return true;
+            return base.CalculateNewGoal(forceStart);
         }
 
         private void ReprioritizeGoals() {
             List<ReGoapGoal<string, object>> goals = new List<ReGoapGoal<string, object>>(GetComponents<ReGoapGoal<string, object>>());
 
             foreach (var goal in goals) {
-                goal.Priority = UnityEngine.Random.Range(1.0f, 10.0f);
+                if (forceWander && goal.Name == "WanderGoal") {
+                    goal.Priority = 100.0f;
+                    forceWander = false;
+                    break;
+                }
+
+                goal.Priority = Random.Range(1.0f, 10.0f);
             }
         }
     }
---
+++ Assets/Unity/FSMExample/Sensors/ScaryAgentSensor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using ReGoap.Unity.FSMExample.OtherScripts;
+using UnityEngine;
+
+namespace ReGoap.Unity.FSMExample.Sensors
+{
+    public class ScaryAgentSensor : ReGoapSensor<string, object>
+    {
+        private GameObject scaryAgent;
+
+        void Start() {
+            scaryAgent = GameObject.Find("ScaryAgent");
+        }
+
+        public override void UpdateSensor() {
+            memory.GetWorldState().Set("sqrDistanceToScaryAgent", (scaryAgent.transform.position - transform.position).sqrMagnitude);
+        }
+    }
+}
