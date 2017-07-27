38c94e772e984a1a0c0bd32ea7e4e47f13fc96f4
Shiv Kumar <shivpkumar@gmail.com>
2017-07-26 19:37:36 -0700

    Minions fetch either trees or branches to make wood

--- Assets/Unity/FSMExample/Agents/BuilderAgent.cs
+++ Assets/Unity/FSMExample/Agents/BuilderAgent.cs
@@ -1,6 +1,26 @@
-﻿namespace ReGoap.Unity.FSMExample.Agents
-{
-    public class BuilderAgent : ReGoapAgentAdvanced<string, object>
-    {
+﻿using ReGoap.Unity.FSMExample.Actions;
+
+namespace ReGoap.Unity.FSMExample.Agents {
+
+    public class BuilderAgent : ReGoapAgentAdvanced<string, object> {
+        // TODO: research if it's easy to access all the preconditions/effects of
+        // all the actions on a gameObject
+
+        private CraftRecipeAction[] recipes;
+
+        protected override void Awake() {
+            base.Awake();
+            recipes = GetComponents<CraftRecipeAction>();
+        }
+
+        protected override bool CalculateNewGoal(bool forceStart = false) {
+            System.Random random = new System.Random();
+
+            foreach (CraftRecipeAction recipe in recipes) {
+                recipe.Cost = random.Next(1, 10);
+            }
+
+            return base.CalculateNewGoal(forceStart);
+        }
     }
 }
